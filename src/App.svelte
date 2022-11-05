6114dd99fe67c304fbb92bd18b85e4d3601a8ef9
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-05 14:12:33 +0100

    added navbar

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,11 @@
 <script lang="ts">
   import Footer from "./components/Footer.svelte";
+  import Navbar from "./components/Navbar.svelte";
 </script>
 
 <main>
 </main>
-
+  <Navbar />
   <Footer />
 <style>
 </style>
