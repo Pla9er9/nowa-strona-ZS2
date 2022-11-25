7a2562b9b5bba928f11dd70cad0bb8a794e839ec
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:31:06 +0100

    c matura sub page

---
+++ src/pages/Matura.svelte
@@ -0,0 +1,66 @@
+<script lang="ts">
+    import Wrapper from "../components/Wrapper.svelte";
+</script>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/09/EM-2023-Zalacznik-1a-Deklaracja-A.docx">Deklaracja Maturalna 2022/2023</a>
+
+<br>
+<h1>e-Deklaracja maturalna</h1>
+<Wrapper>
+    <img src="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/09/x2022-09-26_15h53_16.png.pagespeed.ic.z3bjdyUQvj.webp" alt="e-deklaracja maturalna" width="600px">
+    <p>Filmowa instrukcja składania e-deklaracji </p>
+    <a href="https://youtu.be/il2TChf_BfY">https://youtu.be/il2TChf_BfY</a>
+</Wrapper>
+
+
+<br>
+<a href="https://arkusze.pl">Arkusze Egzaminów Maturalnych I Zawodowych</a>
+
+<h2>Oke poznań</h2>
+<Wrapper>
+    <a href="https://www.oke.poznan.pl/cms,152,informacje.htm">Egzamin Maturalny</a>
+
+    <a href="https://www.oke.poznan.pl/cms,2116,_nowy_egzamin_zawodowy.htm">Nowy Egzamin Zawodowy</a>
+
+    <a href="https://www.oke.poznan.pl">OKE Poznań</a>
+
+    <a href="https://zsp2wagrowiec.pl/pliki/matura/Tematy2015.pdf">Język polski</a>
+</Wrapper>
+
+<style>
+    img {
+        margin-top: 50px;
+        margin-bottom: 50px;
+    }
+    a {
+        background-color: #fff;
+        padding: 2px 10px;
+        border-radius: 5px;
+        border-bottom: 2px solid rgb(255, 221, 0);
+        color: rgb(0, 68, 255);
+        font-size: 1.5em;
+        margin-top: 10px;
+        margin-bottom: 10px;
+        word-wrap: normal;
+        text-align: center;
+    }
+    p {
+        text-align: center;
+    }
+    h1 {
+        text-align: center;
+    }
+    @media only screen and (max-width: 1199px) {
+        img {
+            width: 85%;
+        }
+    }
+    @media only screen and (max-width: 654px) {
+        a {
+            font-size: 1em;
+        }
+        p {
+            font-size: 0.7em;
+        }
+    }
+</style>
