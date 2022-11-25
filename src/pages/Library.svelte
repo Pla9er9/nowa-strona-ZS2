5c757309b212e594e222728a59c856f9b380315f
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:40:35 +0100

    created Library page

---
+++ src/pages/Library.svelte
@@ -0,0 +1,161 @@
+<h1>O bibliotece</h1> 
+<div class="wrapper">
+    <p>W skład Biblioteki Szkolnej wchodzi wypożyczalnia, czytelnia oraz Internetowe Centrum Informacji Multimedialnej (ICIM), które jest miejscem dostępu do Internetu oraz zasobów edukacyjnych i źródeł informacji.</p>
+
+    <p>Można tu również korzystać z sieci Wi-Fi.</p>
+
+    <img src="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2019/03/xObraz1.png.pagespeed.ic.lIs67UinZ4.webp" alt="biblioteka" width="500px">
+
+    <p>Pomieszczenie biblioteki jest słoneczne i przestronne, zapewnia dobre warunki do pracy indywidualnej i grupowej, a także umożliwia korzystanie ze wszystkich zgromadzonych tam dokumentów.</p>
+
+    <p>Warsztat informacyjno-bibliograficzny, stanowią właściwie opracowane  i aktualne katalogi biblioteczne oraz kartoteki dostosowane do potrzeb środowiska szkolnego. Można również korzystać z komputerowych baz danych i multimedialnych źródeł informacji.</p>
+</div>
+
+<br>
+
+
+<h1>Zbiory</h1>
+<div class="wrapper">
+    <p>Stan księgozbioru do dnia 31.12.2018 wynosił 15.939 woluminów o wartości 49.539,49 zł. Jest to literatura piękna (powieści obyczajowe, sensacje, horrory, fantastyka, kryminały) oraz popularno-naukowa.</p>
+
+    <p>W bibliotece można korzystać również ze zbiorów specjalnych. Są to filmy, audiobooki oraz różnego rodzaju wydawnictwa popularno-naukowe.</p>
+
+    <p>Od 13.10.2005 roku Biblioteka szkolna nosi imię dr. Tadeusza Nożyńskiego.</p>
+
+    <img src="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2018/10/xMEN-e1538640429788.png.pagespeed.ic.t9SBgZQ_AS.webp" alt="narodowy program rozwoju czytelnika">
+
+    <p>W czerwcu 2018 r. nasza szkoła otrzymała 15.000 złotych na zakup książek do Biblioteki Szkolnej w ramach Narodowego Programu Rozwoju Czytelnictwa na lata 2016-2020. Priorytet 3: Rozwijanie zainteresowań uczniów przez promowanie i wspieranie rozwoju czytelnictwa wśród dzieci i młodzieży, w tym zakup nowości wydawniczych.</p>
+
+    <p>Warunkiem przyznania wsparcia było zapewnienie przez organ prowadzący szkołę co najmniej 20% wkładu własnego na realizację zakupów. Otrzymaliśmy wsparcie finansowe dla Biblioteki Szkolnej w kwocie 15.000 złotych, z czego 12.000 pochodzi z dotacji, a 3.000 to wkład własny otrzymany od Powiatu Wągrowieckiego.</p>
+
+    <p>Za przyznaną kwotę zostało zakupionych 567 książek (fantastyka, kryminały, sensacje, horrory, powieści obyczajowe, biografie, książki w języku angielskim itp. oraz lektury, które znajdą się w nowej podstawie programowej), a także 12 audiobooków. Uwzględniono również potrzeby uczniów z niepełnosprawnościami.</p>
+
+    <p>W wyniku realizacji projektu księgozbiór Biblioteki Szkolnej znacznie się wzbogacił. Przy zakupie nowości wydawniczych uwzględniono życzenia uczniów i nauczycieli, a regały z nowymi książkami cieszą się ogromną popularnością wśród czytelników.</p>
+
+    <p>Obok zakupu książek zaplanowano szereg inicjatyw promujących czytelnictwo w szkole oraz w środowisku lokalnym.</p>
+
+    <p>Pamiętajmy, że czytanie: poprawia pamięć, ćwiczy mózg, wzbogaca słownictwo, uczy wyrażania uczuć, pomaga zrozumieć rzeczywistość, uczy rozumieć innych ludzi, dostarcza wiedzy, uczy przyswajania nowych wiadomości, ułatwia naukę, rozwija emocjonalnie, poprawia koncentrację, rozwija umiejętności komunikacyjne.</p>
+
+    <p>Zachęcamy do korzystania z księgozbioru Biblioteki Szkolnej.</p>
+</div>
+
+<br>
+
+<h1>Katalog on-line</h1>
+<div class="wrapper">
+    <p>Tutaj możesz przejrzeć jakie książki znajdują się w Bibliotece Szkolnej. Skorzystaj z zamieszczonego linku.</p>
+
+    <a href="https://m008727.molnet.mol.pl">https://m008727.molnet.mol.pl</a>
+
+    <h2>UWAGA!</h2>
+
+    <p>Objętość naszego katalogu będzie się stale powiększać, ponieważ trwają prace nad  jego  tworzeniem.</p>
+
+    <p>Aby przejrzeć zbiory naszej biblioteki nie musisz się logować. Jeśli chcesz natomiast zamówić lub zarezerwować jakąś książkę musisz założyć sobie konto. W tym celu przyjdź do Biblioteki Szkolnej i podaj swój adres e-mail bibliotekarzowi. Potem skorzystaj z poniższej instrukcji:</p>
+
+    <h2>Instrukcja</h2>
+
+    <p>Możesz też obejrzeć poradnik w formie wideo:</p>
+
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/I3Y15UrtPUA" title="Filmik instruktażowy: MOL NET+" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <p>W razie pytań lub problemów z pierwszym logowaniem, przyjdź do Biblioteki Szkolnej. Chętnie pomożemy 🙂</p>
+</div>
+<br>
+
+<h1>Godziny otwarcia</h1>
+<div class="wrapper">
+    <p>Czas otwarcia biblioteki dostosowany jest do rozkładu zajęć szkolnych i umożliwia maksymalne wykorzystanie biblioteki i jej zbiorów przez wszystkich użytkowników.</p>
+
+    <p>Biblioteka jest czynna od poniedziałku do piątku od 7.15 do 15.00.</p>
+</div>
+<br>
+
+<h1>Kontakt</h1>
+<div class="wrapper">
+    <pre><p>Biblioteka Szkolna im. dr. Tadeusza Nożyńskiego
+
+    II piętro pokój 202 – 203
+    
+    bibliotekaszkolna.zs2@o2.pl
+    
+    Bibliotekarze:
+    
+    mgr Beata Schwermer
+    
+    mgr Magdalena Woźniak</p></pre>
+</div>
+<br>
+
+<h1>Video blog</h1>
+<div class="wrapper">
+    <h2>Za Siedmioma Półkami S01E01 „Więzień labiryntu”</h2>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/05Vixl5bH00" title="Za siedmioma półkami S01E01 Więzień labiryntu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/QajerwRTGuc" title="Za Siedmioma Półkami S01E02 Hobbit" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/mgAcRWXym0I" title="Chłopiec w pasiastej piżamie S01E03" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/eZdCeP3Vo8k" title="Za siemioma półkami S01E04 Elementals" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/VB-C4ywIWbY" title="Za siedmioma półkami: Zostań, jeśli kochasz" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/097u5UjPNvc" title="Za siedmioma półkami: Czerwień rubinu" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+
+    <iframe width="603" height="394" src="https://www.youtube.com/embed/4x7-7DjAPEY" title="Za siedmioma półkami: Notatki samobójcy" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+</div>
+<br>
+
+<style>
+    p {
+        border-left: solid 3px #ffb310;
+        padding-left: 20px;
+        max-width: 800px;
+        word-wrap: normal;
+        margin-left: 50px;
+        font-weight: 600;
+    }
+
+    h2 {
+        color: #132a6d;
+        text-align: center;
+    }
+
+    img {
+        border-left: solid 3px #1050ff;
+        padding-left: 20px;
+        width: 70%;
+        margin-top: 50px;
+        margin-bottom: 50px;
+        margin-left: 50px;
+    }
+
+    iframe {
+        margin-top: 50px;
+        margin-bottom: 50px;
+        border-radius: 10px;
+        margin-left: calc(50% - 301px);
+    }
+
+    .wrapper {
+        background-color: #f7f5f5;
+        width: 900px;
+        border-radius: 10px;
+        display: flex;
+        justify-content: center;
+        flex-direction: column;
+    }
+
+    .wrapper a {
+        text-align: center;
+    }
+
+    @media only screen and (max-width: 1200px) {
+        .wrapper {
+            width: 70%;
+        }
+    }
+
+</style>
