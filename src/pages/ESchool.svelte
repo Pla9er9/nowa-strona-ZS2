81432121a94a755afb78a8f2232ca7d0948d24f2
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:41:00 +0100

    c eSchool page

---
+++ src/pages/ESchool.svelte
@@ -0,0 +1,87 @@
+<div id="container">
+
+    <div class="socialMediaLink" style="background: rgb(247,167,47);background: linear-gradient(90deg, rgba(247,167,47,1) 0%, rgba(245,7,254,1) 0%, rgba(242,244,30,1) 100%);">
+        <a href="https://www.facebook.com/techinfzs2/" target="_blank" rel="noreferrer">Technik informatyk</a>
+    </div>
+
+    <div class="socialMediaLink" style="background: rgb(14,0,255);background: linear-gradient(90deg, rgba(14,0,255,1) 0%, rgba(18,254,7,1) 0%, rgba(30,108,244,1) 100%);">
+        <a href="https://www.facebook.com/ekonomiscizspnr2/" target="_blank" rel="noreferrer">Technik ekonomista</a>
+    </div>
+    
+    <div class="socialMediaLink" style="background: rgb(254,7,246);background: linear-gradient(90deg, rgba(254,7,246,1) 0%, rgba(254,7,7,1) 0%, rgba(60,30,244,1) 100%);">
+        <a href="https://www.facebook.com/najlepszehityzs2/" target="_blank" rel="noreferrer">Technik hotelarstwa</a>
+    </div>
+
+    <div class="socialMediaLink"  style="background: rgb(62,7,254);background: linear-gradient(90deg, rgba(62,7,254,1) 0%, rgba(54,7,254,1) 0%, rgba(30,237,244,1) 100%);">
+        <a href="https://www.facebook.com/profile.php?id=100069886475302" target="_blank" rel="noreferrer">Bezpieczeństwo Publiczne</a>
+    </div>
+
+
+    <div class="socialMediaLink" style="background: rgb(247,167,47);
+    background: linear-gradient(90deg, rgba(247,167,47,1) 0%, rgba(245,7,254,1) 0%, rgba(242,244,30,1) 100%);">
+    <a href="https://www.facebook.com/profile.php?id=100029445223447&fref=ts" target="_blank" rel="noreferrer">Klasa politechniczna</a>
+    </div>
+    
+    <div class="socialMediaLink" style="background: rgb(62,7,254);
+        background: linear-gradient(90deg, rgba(62,7,254,1) 0%, rgba(54,7,254,1) 0%, rgba(30,237,244,1) 100%);">
+        <a href="https://azureforeducation.microsoft.com/devtools" target="_blank" rel="noreferrer">Azure dla zs2 Rejestracja</a>
+    </div>
+
+
+    <div class="socialMediaLink" style="background: rgb(14,0,255);
+    background: linear-gradient(90deg, rgba(14,0,255,1) 0%, rgba(18,254,7,1) 0%, rgba(30,108,244,1) 100%);">
+        <a href="https://portal.azure.com" target="_blank" rel="noreferrer">Azure dla zs2 logowanie</a>
+    </div>
+
+
+    <div class="socialMediaLink">
+        <a href="https://www.youtube.com/channel/UCiutxe0og69YOmRjYkRg_Fw/feed?view_as=public" target="_blank" rel="noreferrer">Youtube zs2</a>
+    </div>
+
+</div>
+
+<style>
+    a{
+        color: rgb(255, 255, 255);
+        font-family: 'Franklin Gothic Medium', 'Arial Narrow', Arial, sans-serif;
+        font-size: 18px;
+    }
+
+    #container {
+        display: flex;
+        justify-content: space-around;
+        align-items: center;
+        flex-wrap:wrap;
+        width: 1000px;
+        margin-top: 50px;
+    }
+
+    .socialMediaLink {
+        width: 300px;
+        height: 100px;
+        background-color: rgb(246, 74, 74);
+        border: solid 1px none;
+        box-shadow: 2px 2px 3px rgb(0, 0, 0);
+        border-radius: 25px;
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        margin-bottom: 100px;
+    }
+
+    @media only screen and (max-width: 1389px) {
+        #container {
+            width: 800px;
+        }
+    }
+
+    @media only screen and (max-width: 799px) {
+        #container {
+            width: 300px;
+        }
+        .socialMediaLink {
+            width: 250px;
+        }
+    }
+
+</style>
