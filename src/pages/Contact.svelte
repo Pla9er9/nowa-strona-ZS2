decbb886371ab4554c9134460f7896eae848ab65
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:41:35 +0100

    c Contact page

---
+++ src/pages/Contact.svelte
@@ -0,0 +1,56 @@
+<script lang="ts">
+  import ShowTextBtn from '../components/ShowTextBtn.svelte';
+</script>
+
+<ShowTextBtn title="Dane Teleadresowe" description="
+    Zespół Szkół nr 2 <br>
+    im. ppłk. dr. Stanisława Kulińskiego <br>
+    ul. Kościuszki 49 <br>
+    62-100 Wągrowiec <br>
+    📞 tel. (67) 2620979 <br>
+    📞 tel.kom. 605040076 <br>
+    📞 tel./fax. (67)2620347 <br>
+    📧 e-mail: zsp2@wagrowiec.pl">
+</ShowTextBtn>
+
+<ShowTextBtn title="Dyrekcja" description="
+Gabinety Dyrekcji Szkoły:
+I piętro pok.101 <br>
+📞 605040076 <br>
+📧 i.synoradzka@zsp2wagrowiec.pl <br>
+Poniedziałek: 8:00 – 13:00 <br>
+Wtorek: 8:00 – 14:00 <br>
+Środa: 9:00 – 12:00 <br>
+Czwartek: 8:00 – 14:00 <br>
+Piątek: 8:00 – 12:00 <br>
+">
+
+</ShowTextBtn>
+
+<ShowTextBtn title="Sekretariat" description="
+Sekretariat: I piętro pok. 102 <br>
+📞 (67) 2620347 <br>
+Czynny od poniedziałku do piątku
+w godzinach <br> – 7:30 ÷ 15:30 <br>
+Dla Interesantów czynny: <br>
+- Poniedziałek: 7:30 – 12:00 <br>
+- Wtorek: 7:30 – 15:30 <br>
+- Środa: 7:30 – 15:30 <br>
+- Czwartek: 7:30 – 15:30 <br>
+- Piątek: 7:30 – 12:00 <br>
+"></ShowTextBtn>
+
+<ShowTextBtn title="Pedagog" description="
+DYŻURY PSYCHOLOGA I PEDAGOGA SZKOLNEGO <br>
+PSYCHOLOG SZKOLNY mgr Urszula Francuziak (sala 105) <br>
+<img src='https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/10/x2022-10-05_17h14_20-1024x487.png.pagespeed.ic.bLLzh0RO1O.webp' style='width:80%;min-with:150px'>
+"></ShowTextBtn>
+
+<ShowTextBtn title="Koordynator Do Spraw Dostępu" description="
+Koordynator do spraw dostępu: <br>
+Aleksandra Szulczewska <br>
+📧 e-mail: a.szulczewska@zsp2wagrowiec.pl <br>
+📞 Numer telefonu: (67) 262-03-47 <br>
+"></ShowTextBtn>
+
+<ShowTextBtn title="Deklaracja Dostępności" description='<a href="/docs/dd.pdf">Deklaracja dostępności</a>'></ShowTextBtn>
