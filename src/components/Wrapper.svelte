1b1dc06917152dcd2e75b6a21b19f71cbb3abcc1
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 19:07:15 +0100

    c wrapper

---
+++ src/components/Wrapper.svelte
@@ -0,0 +1,16 @@
+<div class="wrapper">
+    <slot></slot>
+</div>
+
+<style>
+    .wrapper {
+        background-color: #f7f5f5;
+        width: 900px;
+        border-radius: 10px;
+        margin-bottom: 50px;
+        display: flex;
+        flex-direction: column;
+        align-items: center;
+        height: max-content;
+    }
+</style>
