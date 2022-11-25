e65f07ef8e3cfc1149e4c01812ded2eadfe261cd
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 19:05:59 +0100

    c project link components

---
+++ src/components/ProjectLink.svelte
@@ -0,0 +1,16 @@
+<div class="projectLink">
+    <slot></slot>
+</div>
+
+<style>
+    .projectLink {
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        background-color: #4a55ff;
+        width: 400px;
+        height: 50px;
+        border-radius: 15px;
+    }
+
+</style>
