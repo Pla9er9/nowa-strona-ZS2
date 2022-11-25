6d450e2d2fed8095981438999cf3bb78f4fee9d1
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:30:11 +0100

    c Rodo sub page

---
+++ src/pages/Rodo.svelte
@@ -0,0 +1,39 @@
+<h2>Inspektor ochrony danych</h2>
+<img src="images/iod.webp" alt="inspektor ochrony danych" width="55%" style="border: solid 2px black;border-radius:15px">
+<br>
+
+<h2>Klauzula informacyjna</h2>
+<iframe src="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/10/KLAUZULA-INFORMACYJNA-na-strone.pdf" width="1000px" height="800px" title="Klauzula informacyjna
+" style="border: solid 2px black;border-radius:15px"></iframe>
+
+<style>
+    h2 {
+        font-size: 2em;
+    }
+    img {
+        max-width: 1000px;
+    }
+    @media only screen and (max-width: 680px){
+        img {
+            width: 90%;
+        }
+        iframe {
+            width: 90%;
+        }
+        h2 {
+            font-size: 1.2em;
+        }
+    }
+
+    @media only screen and (max-width: 1383px){
+        img {
+            width: 80%;
+        }
+        iframe {
+            width: 80%;
+        }
+    }
+    iframe {
+        margin-bottom: 150px;
+    }
+</style>
