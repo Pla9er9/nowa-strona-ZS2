4dc5c3c57a21a78349a59ffc2e16cc4e901ce16e
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-11-25 18:29:47 +0100

    Created school sub page

---
+++ src/pages/School.svelte
@@ -0,0 +1,5 @@
+<p>szkola</p>
+
+<a href="https://zsp2wagrowiec.pl/pliki/2021/Statut%202021.pdf">Status szkoły</a>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/10/Kalendarz-22_23.pdf">Kalendarz </a>
