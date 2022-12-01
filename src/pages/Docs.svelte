5f35f530c7291ca7f09fa3ba42ab4d2a83dfe658
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-12-01 01:42:16 +0100

    c docs page

--- src/pages/Docs.svelte
+++ src/pages/Docs.svelte
@@ -1,1 +1,81 @@
-<p>dokumenty</p>
+<script lang="ts">
+    import ShowTextBtn from "../components/ShowTextBtn.svelte";
+</script>
+
+<ShowTextBtn title="Statut 2021" description='
+    <a href="docs/Statut-2021.pdf">Statut 2021</a> 
+'></ShowTextBtn>
+
+<ShowTextBtn title="Kalendarz 22/23" description='
+    <a href="docs/Kalendarz-22_23.pdf">Kalendarz 22/23</a>
+'></ShowTextBtn>
+
+<ShowTextBtn title="Duplikaty Dokumentów" description='
+    <a href="docs/zasady-duplikat.pdf">zasady duplikat</a> <br>
+    <a href="docs/wniosek.pdf">wniosek</a>
+'></ShowTextBtn>
+
+<ShowTextBtn title='Komitet rodzicielski' description='<a href="docs/KOMITET.pdf">Komitet rodzicielski</a>'></ShowTextBtn>
+
+<ShowTextBtn title='PWP 2022-2027' description='<a href="docs/P-P-W-2022-2027.pdf">Program wychowawczo-profilaktyczny na lata 2022-2027</a>'></ShowTextBtn>
+
+
+
+
+
+
+<ShowTextBtn title="PSO" description='
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_j_polski.pdf">Język Polski</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_j_angielski.pdf">Język Angielski</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_j_niemiecki_j_hiszpanski_j_rosyjski.pdf">Język Niem./Hiszp./Ros.</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_historia_-wok_itp.pdf">Historia</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_fizyka_PP_i_PR.pdf">Fizyka</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_przedmioty_ekonomiczne_i_pp.pdf">Przedmioty Ekonomiczne</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_przedmioty_ekonomiczne_i_pp.pdf">Podstawy Przedsiębiorczości</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_bp.pdf">Bezpieczeństwo Publiczne</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_chemia.pdf">Chemia</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_edb.pdf">Edukacja Dla Bezpieczeństwa</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/pliki/pso/PSOekonomiawpraktyce.pdf">Ekonomia W Praktyce</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_geografia.pdf">Geografia</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_matematyka.pdf">Matematyka</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/pliki/pso/PSOprzedmiotyprzyrodnicze.pdf">Przedmioty Przyrodnicze</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2021/09/PSO_informatyka.pdf">Przedmioty Informatyczne</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/pliki/pso/PSOwos.pdf">Wiedza O Społeczeństwie</a> <br>
+
+<a href="https://zsp2wagrowiec.pl/pliki/pso/PSOwychowaniefizyczne.pdf">Wychowanie Fizyczne</a> <br>
+'></ShowTextBtn>
+
+
+<ShowTextBtn title='PPP regulamin' description='<a href="docs/PPP-Regulamin.pdf">PPP Regulamin</a>'></ShowTextBtn>
+
+
+<ShowTextBtn title='Zasady Organizacji Wycieczek Szkolnych' description='
+    <a href="docs/PROCEDURA-PRZEKAZYWANIA-I-WPROWADZANIA-DOSTOSOWAN.pdf">Zasady Organizacji Wycieczek Szkolnych</a>
+'></ShowTextBtn>
+
+<ShowTextBtn title="ZFŚS ZS 2" description='
+    <a href="docs/Regulamin-ZFSS-2021-1.docx">Regulamin ZFŚS 2022</a> <br>
+
+    <a href="docs/zalacznik_3.pdf">Oświadczenie o sytuacji życiowej</a> <br>
+
+    <a href="docs/Wniosek_o_pozyczke.pdf">Wniosek o pozyczke</a> <br>
+
+    <a href="docs/Wniosek_o_przyznanie_swiadczenia.pdf">Wniosek O Przyznanie Świadczenia</a> <br>
+
+    <a href="docs/ZFSS_klauzula_informacyjna.pdf">Klauzula Informacyjna</a>
+'></ShowTextBtn>
