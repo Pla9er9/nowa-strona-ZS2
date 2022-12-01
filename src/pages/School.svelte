d0ba292e29e2e7a7cb085e9190eacd1e016434e2
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-12-01 01:41:11 +0100

    created school page

--- src/pages/School.svelte
+++ src/pages/School.svelte
@@ -1,5 +1,51 @@
-<p>szkola</p>
+<script>
+    import ShowTextBtn from "../components/ShowTextBtn.svelte";
+</script>
 
-<a href="https://zsp2wagrowiec.pl/pliki/2021/Statut%202021.pdf">Status szkoły</a>
 
-<a href="https://zsp2wagrowiec.pl/cms/wp-content/uploads/2022/10/Kalendarz-22_23.pdf">Kalendarz </a>
+<ShowTextBtn title="O Nas" description='
+<h2>Dyrekcja</h2>
+<pre><p>
+Dyrektor Szkoły  
+mgr Iwona Muszyńska-Synoradzka 
+
+Wicedyrektor  
+mgr Katarzyna Resztak
+
+Wicedyrektor    
+mgr Lucyna Żerkowska-Chełminiak
+</p></pre>
+
+<a href="docs/nauczyciele.pdf"><h2>Nauczyciele</h2></a>
+
+<a href="https://zsp2wagrowiec.pl/pliki/Prezentacja.pdf"><h2>Typy szkół</h2></a>
+
+<a href="docs/patron.pdf"><h2>Patron</h2></a>
+
+<a href="docs/historia.pdf"><h2>Historia szkoły</h2></a>
+
+<a href="https://youtu.be/i-Pe6jqsuiQ"><h2>Wirtualny spacer</h2></a>
+'></ShowTextBtn>
+
+<ShowTextBtn title="Podręczniki" description='
+    <a href="https://docs.google.com/spreadsheets/d/1WXOmbUDM0tKl5ZjeRIB4u9uMqlUT0Pjz/edit?usp=sharing&ouid=113821945758794080291&rtpof=true&sd=true">Podręczniki Od Roku Szkolnego 2022/2023</a>
+'></ShowTextBtn>
+
+<ShowTextBtn title="Konkursy" description='
+    <pre><h2>Biblioteka</h2> 
+    <a href="docs/konkurs-plastyczno-literacji-oswiadczenie.pdf">Klauzula informacyjna</a> 
+    <a href="docs/biblioteka_PDO.pdf">Przetwarzanie danych osobowych</a> 
+    <a href="docs/REGULAMIN-KONKURSU-LITERACKO-PLASTYCZNEGO.pdf">Regulamin Konkursu literacko-plastycznego</a> 
+    <a href="docs/REGULAMIN-KONKURSU-PLASTYCZNEGO.pdf">Regulamin konkursu plastycznego</a>    
+    <h2>Chemiczny</h2> 
+    <a href="docs/Klauzula-informacyjna-chemia.pdf">Klauzula Informacyjna</a> 
+    <a href="docs/Regulamin-konkursu-chemicznego.pdf">Regulamin</a> 
+
+    <h2>Fizyczny</h2> 
+    <a href="docs/Klauzula-informacyjna-fizyka-2022.pdf">Klauzula Informacyjna</a> 
+    <a href="docs/Regulamin-konkursu-z-fizyki-2022-1.pdf">Regulamin</a></pre>
+'></ShowTextBtn>
+
+<ShowTextBtn title="Czas zawodowców" description='
+<a href="docs/czas-zawodowców.pdf">Czas zawodowców</a>
+'></ShowTextBtn>
