55bf8d8796d510f13684622e4a10833babc6eabc
Dawid <98473383+Pla9er9@users.noreply.github.com>
2022-12-01 01:41:26 +0100

    updated

--- src/pages/News.svelte
+++ src/pages/News.svelte
@@ -1,49 +1,48 @@
 <script lang="ts">
+  import App from "../App.svelte";
   import Post from "../components/Post.svelte";
+  import Contact from "./Contact.svelte";
 </script>
 <div>
-
-  <Post title="Drzwi otwarte" image="news/do.webp" 
-  description=""
-  ></Post>
-
-  <Post title="Szkoła do hymnu" image="news/sdh.webp" 
-  description="Tak jak w ostatnich latach również w tym roku tuż przed Narodowym Świętem Niepodległości uczniowie i nauczyciele wzięli udział w Akcji „Szkoła do Hymnu”. Symbolicznie o godz. 11.11 wszyscy wspólnie odśpiewali pełną 4-zwrotkową wersję „Mazurka Dąbrowskiego”."
-  ></Post>
-
-  <Post title="STRONA szkolna – konkurs" image="news/konkurs.webp" 
-  description="Przygotuj projekt strony szkolnej, która mogłaby zastąpić obecną www.zsp2wagrowiec.pl
-  Wymagania:
   
-  atrakcyjny design;
-  skalowalność;
-  przejrzystość i czytelność;
-  projekt własny.
-  Inne:
-  
-  wstępna grafika może być do zastąpienia;
-  unikać należy technologii nie wspieranych (np. Flash);
-  technologia może być dowolna;
-  oryginalność mile widziana;
-  warto wykorzystać oficjalne logo ZS2;
-  warto przyjrzeć się obecnemu menu i spróbować je zaadoptować ale można eksperymentować;
-  mile widziany dział BIP (obecnie jest link odsyłający do BIP starostwa a ma być własny BIP szkoły);
-  warto pamiętać, że taki serwis będzie uzupełniany/rozwijany zatem mile widziany „system wpisów”.
-  Wystarczy dołączyć do Classroom (kod na obrazku). W niektórych klasach (informatycznych/programistycznych) udział w tym konkursie jest obligatoryjny. Może się zdarzyć jednak, że jest ktoś, kto ma talent, a nie jest w takiej klasie – stąd ogłoszenie na tej stronie.
-  
-  Termin – (wstępnie) czekamy do końca listopada.
-  
-  Nagrody – najlepszą nagrodą będzie samo zwycięstwo w konkursie 🙂 dla wielu uczniów uczestniczących z obowiązku będzie ocena, ale nie jest wykluczone, że pojawią się jakieś specjalne nagrody."
-  ></Post>
+  <Post title="Drzwi otwarte" description="" image="news/do.webp"></Post>
 
-  <Post title="Wojskowa Gra Terenowa" image="news/wgt.jpg" 
-  description="Ministerstwo Obrony Narodowej – organizuje 11.11.2022 w Jarocinie „Wojskową Grę Terenową”. Szczegóły na stronie www.mon.gov.pl"
-  ></Post>
+  <Post title='Szkoła do hymnu' description='
+  Tak jak w ostatnich latach również w tym roku tuż przed Narodowym Świętem Niepodległości uczniowie i nauczyciele wzięli udział w Akcji „Szkoła do Hymnu”. Symbolicznie o godz. 11.11 wszyscy wspólnie odśpiewali pełną 4-zwrotkową wersję „Mazurka Dąbrowskiego”.
+  ' image="news/sdh.webp"></Post>
 
-  <Post title="II miejsce szachistów" image="news/ms.webp" 
-  description="4. listopada w Szkole Podstawowej nr 2 w Wągrowcu odbyły Mistrzostwa Powiatu SZS w Szachach Drużynowych. W składzie reprezentacji naszej szkoły wystąpili: Maja Ciesiółka, Michał Kubanek, Szymon Lihs, Radosław Wieczorek, Bartłomiej Cholewiński i Mikołaj Grewling. Tym razem bezpośredni pojedynek z najgroźniejszym rywalem miał miejsce już w pierwszej rundzie (w ubiegłym roku miało to miejsce w ostatniej rundzie). Wynik tego meczu był remisowy 2:2 ze wskazaniem na ZS2, bo zdobyliśmy punkty na pierwszych dwóch szachownicach. Po dziesięciu rundach okazało się, że minimalne straty w małych punktach „zepchnęły” naszych na 2. miejsce. Zacięta walka trwała ponad cztery godziny. Drugie miejsce też dało awans do rozgrywek na szczeblu wojewódzkim. Gratulacje!"
-  ></Post>
+  <Post title='STRONA szkolna – konkurs' description='
+  Przygotuj projekt strony szkolnej, która mogłaby zastąpić obecną www.zsp2wagrowiec.pl
+Wymagania:
 
+atrakcyjny design;
+skalowalność;
+przejrzystość i czytelność;
+projekt własny.
+Inne:
 
-</div>
+wstępna grafika może być do zastąpienia;
+unikać należy technologii nie wspieranych (np. Flash);
+technologia może być dowolna;
+oryginalność mile widziana;
+warto wykorzystać oficjalne logo ZS2;
+warto przyjrzeć się obecnemu menu i spróbować je zaadoptować ale można eksperymentować;
+mile widziany dział BIP (obecnie jest link odsyłający do BIP starostwa a ma być własny BIP szkoły);
+warto pamiętać, że taki serwis będzie uzupełniany/rozwijany zatem mile widziany „system wpisów”.
+Wystarczy dołączyć do Classroom (kod na obrazku). W niektórych klasach (informatycznych/programistycznych) udział w tym konkursie jest obligatoryjny. Może się zdarzyć jednak, że jest ktoś, kto ma talent, a nie jest w takiej klasie – stąd ogłoszenie na tej stronie.
+
+Termin – (wstępnie) czekamy do końca listopada.
+
+Nagrody – najlepszą nagrodą będzie samo zwycięstwo w konkursie 🙂 dla wielu uczniów uczestniczących z obowiązku będzie ocena, ale nie jest wykluczone, że pojawią się jakieś specjalne nagrody.
+  ' image='news/konkurs.webp'></Post>
+
+  <Post title="Wojskowa Gra Terenowa" description='
+  Ministerstwo Obrony Narodowej – organizuje 11.11.2022 w Jarocinie „Wojskową Grę Terenową”. Szczegóły na stronie www.mon.gov.pl
+  ' image="news/wgt.jpg"></Post>
+
+  <Post title='II miejsce ' description='
+  4. listopada w Szkole Podstawowej nr 2 w Wągrowcu odbyły Mistrzostwa Powiatu SZS w Szachach Drużynowych. W składzie reprezentacji naszej szkoły wystąpili: Maja Ciesiółka, Michał Kubanek, Szymon Lihs, Radosław Wieczorek, Bartłomiej Cholewiński i Mikołaj Grewling. Tym razem bezpośredni pojedynek z najgroźniejszym rywalem miał miejsce już w pierwszej rundzie (w ubiegłym roku miało to miejsce w ostatniej rundzie). Wynik tego meczu był remisowy 2:2 ze wskazaniem na ZS2, bo zdobyliśmy punkty na pierwszych dwóch szachownicach. Po dziesięciu rundach okazało się, że minimalne straty w małych punktach „zepchnęły” naszych na 2. miejsce. Zacięta walka trwała ponad cztery godziny. Drugie miejsce też dało awans do rozgrywek na szczeblu wojewódzkim. Gratulacje!
+  ' image='news/ms.webp'></Post>
+
+</div> 
 
